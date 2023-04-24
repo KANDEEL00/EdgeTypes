b50a105096362c650da941a0cc551adf784248fa
Ahmed Kandeel <1kandeel1@gmail.com>
2023-04-25 01:45:44 +0200

    Add project files.

---
+++ EdgeTypes/EdgeTypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem
+{
+    public static class EdgeTypes
+    {
+        #region YOUR CODE IS HERE
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Detect & count the edge types of the given UNDIRECTED graph by applying COMPLETE-DFS on the entire graph 
+        /// NOTE: during search, break ties (if any) by selecting the verices in ASCENDING numeric order
+        /// </summary>
+        /// <param name="vertices">array of vertices in the graph (named from 0 to |V| - 1)</param>
+        /// <param name="edges">array of edges in the graph</param>
+        /// <returns>return array of 3 numbers: outputs[0] number of backward edges, outputs[1] number of forward edges, outputs[2] number of cross edges</returns>
+        public static int[] DetectEdges(int[] vertices, KeyValuePair<int, int>[] edges)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+
+
+        #endregion
+    }
+}
